215b4f25b5bdb2dd8a0a20a413aed61cdfb08105
Ashika1612 <90318556+Ashika1612@users.noreply.github.com>
2023-10-23 21:08:45 +0530

    Update set_operations.py

--- set_operations.py
+++ set_operations.py
@@ -1,13 +1,7 @@
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
 
-E = {0, 2, 4, 6, 8}
-N = {2, 4, 5, 8}
-
-union = E.union(N)
-intersection = E.intersection(N)
-difference = E.difference(N)
-symmetric_difference = E.symmetric_difference(N)
-
-print(f"Union of E and N is {union}")
-print(f"Intersection of E and N is {intersection}")
-print(f"Difference of E and N is {difference}")
-print(f"Symmetric Difference of E and N is {symmetric_difference}")
+print("Union of E and N is",E | N)
+print("Intersection of E and N is",E & N)
+print("Difference of E and N is",E - N)
+print("Symmetric difference of E and N is",E ^ N)
